68e1e8cfbe6581e02cb38fc46945f56c13cd4430
Dan <danhboi23@hotmail.com>
2020-01-16 16:04:51 -0800

    Added houzz function

--- JobsGet.py
+++ JobsGet.py
@@ -12,6 +12,31 @@ import os
 import time
 
 
+def func_houzz(company, driver):
+    driver.get('https://www.houzz.com/jobs?team=Engineering')
+    try:
+        element = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.ID, "Santa Monica, CA, US")))
+    except:
+        print('didnt work')
+        return company + 'didnt work'
+    elem = driver.find_element_by_xpath("//a[@id='Santa Monica, CA, US']/../..")
+    items = elem.find_elements_by_xpath("following-sibling::tr")
+    for e in items:
+        try:
+            if e.get_attribute('class') != '': 
+                break
+            job_title = e.find_element_by_tag_name('span').text
+            job_url = e.find_element_by_tag_name('a').get_attribute('href')
+            # print(job_title)
+            # print(job_url)
+            print('')
+
+            addjob(job_title, company, job_url)
+        except:
+            pass
+
+
 def func_zillow(company, driver):
     driver.get('https://careers.zillowgroup.com/List-Jobs/type/Software-Development/location/Irvine,Los-Angeles,Riverside')
     try:
@@ -143,32 +168,33 @@ def my_function(event, context):
     
     chrome_options = Options()
 
-    #### Comment out when running locally 
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--window-size=1280x1696')
-    chrome_options.add_argument('--user-data-dir=/tmp/user-data')
-    chrome_options.add_argument('--hide-scrollbars')
-    chrome_options.add_argument('--enable-logging')
-    chrome_options.add_argument('--log-level=0')
-    chrome_options.add_argument('--v=99')
-    chrome_options.add_argument('--single-process')
-    chrome_options.add_argument('--data-path=/tmp/data-path')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--homedir=/tmp')
-    chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
-    chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-    chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+    #### Comment out when testing locally 
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--window-size=1280x1696')
+    # chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+    # chrome_options.add_argument('--hide-scrollbars')
+    # chrome_options.add_argument('--enable-logging')
+    # chrome_options.add_argument('--log-level=0')
+    # chrome_options.add_argument('--v=99')
+    # chrome_options.add_argument('--single-process')
+    # chrome_options.add_argument('--data-path=/tmp/data-path')
+    # chrome_options.add_argument('--ignore-certificate-errors')
+    # chrome_options.add_argument('--homedir=/tmp')
+    # chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+    # chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+    # chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
     
     # Functions to target company's Website to scrape data
-    func_ciedigital('ciedigital', driver)
-    func_acorns('acorns', driver)
-    func_ephesoft('ephesoft', driver)
-    func_kareo('kareo', driver)
-    func_zillow('zillow', driver)
+    # func_ciedigital('ciedigital', driver)
+    # func_acorns('acorns', driver)
+    # func_ephesoft('ephesoft', driver)
+    # func_kareo('kareo', driver)
+    # func_zillow('zillow', driver)
+    func_houzz('houzz', driver)
     
     
     # Invoke Lambda for E-mail
@@ -183,4 +209,4 @@ def my_function(event, context):
 
 
 ##### Comment out when running on AWS Lambda
-# my_function('','')
+my_function('','')
